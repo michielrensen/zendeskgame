07fcddf9ed221a355f9d8b94dde327f616159e62
Michiel Rensen <michiel@getseoshop.com>
2015-05-19 16:58:28 +0200

    POC CI + CD

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once '../app.php';
 
 /* Routing */
 
+$app->get('/', function() {
+    return 'Hello';
+});
+
 $app->get('/experience/', 'experience.controller:indexAction');
 $app->get('/experience/show/{userid}', 'experience.controller:showAction');
 $app->get('/experience/update/{userid}/{mutation}', 'experience.controller:updateAction');
